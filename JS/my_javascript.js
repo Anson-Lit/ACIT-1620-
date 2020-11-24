85929d436837c1ffbf96b645839652bba8648488
Anson <ansonlit@live.ca>
2020-11-23 16:12:08 -0800

    add element if user input not found

--- JS/my_javascript.js
+++ JS/my_javascript.js
@@ -16,22 +16,39 @@ function findCourse(courseList) {
         user_input = prompt("Enter a 4 digit code")
     } while (user_input.length !== 4 || isNaN(Number(user_input)))
 
-    let listItems = document.querySelectorAll(".container a")
+    let listItems = document.querySelectorAll(".container")
     check = true
     for (item of listItems) {
-        if (item.innerHTML.includes(user_input)) {
+        if (item.querySelector("a").innerHTML.includes(user_input)) {
             item.style.backgroundColor = "green"
             check = false
         }
     }
+
     if (check) {
-        let para = document.createElement("p")
-        let text = document.createTextNode(`Course code: ${user_input} N/A Fall 2020`)
-        para.appendChild(text)
-        document.body.appendChild(para)
+        let my_container = document.createElement("div")
+        my_container.className = "div-line container"
+
+        let course_name = document.createElement("p")
+        let text_name = document.createTextNode(`Course Code: ${user_input}`)
+        course_name.appendChild(text_name)
+        my_container.appendChild(course_name)
+
+        let para_description = document.createElement("p")
+        let text_description = document.createTextNode("N/A")
+        para_description.appendChild(text_description)
+        my_container.appendChild(para_description)
+
+
+        let para_date = document.createElement("p")
+        let text_date = document.createTextNode("Fall 2020")
+        para_date.appendChild(text_date)
+        my_container.appendChild(para_date)
+
+        document.body.appendChild(my_container)
+
     }
 }
-
 
 let course_array = createCourseArray()
 
